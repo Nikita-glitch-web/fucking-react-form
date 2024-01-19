896b6dca3143e9e1a13090d7a411f43f875c62dc
Nick <nikitalytvynov0506@gmail.com>
2024-01-19 19:16:38 +0200

    first-commit

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "../styles/Register.module.css";
+
+function Register() {
+  return (
+    <div className={style.page_wrapper}>
+      <h1 className={style.title}>Author is gay</h1>
+    </div>
+  );
+}
+
+export default Register;
